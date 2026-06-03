0a468d6c7f7f29ae7b32a63d00f7ecb6f0d06669
Torben <59419684+entorb@users.noreply.github.com>
2026-06-03 08:34:43 +0200

    Update route-map.d.ts

--- src/route-map.d.ts
+++ src/route-map.d.ts
@@ -20,9 +20,9 @@ import type {
 
 declare module 'vue-router' {
   interface TypesConfig {
-    _ParamParsers: {}
+    ParamParsers:
+      | never
     RouteNamedMap: import('vue-router/auto-routes').RouteNamedMap
-    _RouteFileInfoMap: import('vue-router/auto-routes')._RouteFileInfoMap
   }
 }
 
@@ -63,15 +63,11 @@ declare module 'vue-router/auto-routes' {
         | '/ConfigPage'
       views:
         | never
-      pathParamNames:
-        | never
     }
     'src/pages/HomePage.vue': {
       routes:
         | '/HomePage'
       views:
-        | never
-      pathParamNames:
         | never
     }
   }
